766be9d723ddacb66e4f4866859ae62c5fb0fa5b
DELL <DELL@DELL-PC>
2019-01-20 15:24:15 +0530

    First commit uploading all files

---
+++ Sample/src/main/java/com/in28minutes/springboot/WelComeController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelComeController {
+	
+	/*@Autowired
+	WelcomeService ws;
+			
+	
+	@RequestMapping("/welcome")
+	
+	public String welcome() {
+		
+		return ws.Retvmsg();
+	}
+*/
+}
